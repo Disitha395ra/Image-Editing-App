b66d9ac7e48f7b4557530d9aac3018d9dd576154
Disitha <disithar@gmail.com>
2025-01-10 22:56:29 +0530

    Add button to Home component for user interaction

--- EdittingApp/components/Home.js
+++ EdittingApp/components/Home.js
@@ -1,21 +1,38 @@
 import {StyleSheet, ScrollView,Image} from 'react-native'
-import { PaperProvider,Text } from 'react-native-paper'
+import { PaperProvider, Text, Button } from "react-native-paper";
 export default function Home(){
     return (
-        <PaperProvider>
-            <ScrollView>
-                <Image
-                source={require('../assets/banner.jpg')}
-                style={styles.banner}
-                />
-            </ScrollView>
-        </PaperProvider>
-    )
+      <PaperProvider>
+        <ScrollView>
+          <Image
+            source={require("../assets/banner.jpg")}
+            style={styles.banner}
+          />
+          <Button
+            icon="airballoon-outline"
+            mode="contained"
+            onPress={() => console.log("Pressed")}
+            style={styles.homebutton}
+          >
+            Start Here
+          </Button>
+        </ScrollView>
+      </PaperProvider>
+    );
 }
 
 const styles = StyleSheet.create({
-  banner:{
-    width: '600',
+  banner: {
+    width: "600",
     height: 900,
+  },
+  homebutton:{
+    position: "absolute",
+    alignItems: "center",
+    justifyContent: "center",
+    width: 200,
+    height: 60,
+    top: 400,
+    left: 110,
   }
 });
