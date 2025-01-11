44986e767f0ae5252454b85ac82bc53aea74a7d8
Disitha <disithar@gmail.com>
2025-01-11 11:32:37 +0530

    Add tint buttons to Tint component for color selection

--- EdittingApp/components/Tint.js
+++ EdittingApp/components/Tint.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { StyleSheet, View, Image } from "react-native";
-import { PaperProvider, Text } from "react-native-paper";
+import { PaperProvider, Text, IconButton, MD3Colors } from "react-native-paper";
 
 export default function Tint({ route }) {
   const { photo } = route.params || {};
@@ -13,6 +13,53 @@ export default function Tint({ route }) {
         ) : (
           <Text>No photo provided</Text>
         )}
+        <View style={{ flexDirection: "row", marginTop: 50 }}>
+          <View style={styles.buttoncomponent}>
+            <IconButton
+              icon="camera"
+              iconColor={MD3Colors.error50}
+              size={20}
+              onPress={() => console.log("Pressed")}
+            />
+            <Text style={styles.buttontext}>Red Tint</Text>
+          </View>
+          <View style={styles.buttoncomponent}>
+            <IconButton
+              icon="camera"
+              iconColor={MD3Colors.error50}
+              size={20}
+              onPress={() => console.log("Pressed")}
+            />
+            <Text style={styles.buttontext}>Green Tint</Text>
+          </View>
+          <View style={styles.buttoncomponent}>
+            <IconButton
+              icon="camera"
+              iconColor={MD3Colors.error50}
+              size={20}
+              onPress={() => console.log("Pressed")}
+            />
+            <Text style={styles.buttontext}>Blue Tint</Text>
+          </View>
+          <View style={styles.buttoncomponent}>
+            <IconButton
+              icon="camera"
+              iconColor={MD3Colors.error50}
+              size={20}
+              onPress={() => console.log("Pressed")}
+            />
+            <Text style={styles.buttontext}>Yellow Tint</Text>
+          </View>
+          <View style={styles.buttoncomponent}>
+            <IconButton
+              icon="camera"
+              iconColor={MD3Colors.error50}
+              size={20}
+              onPress={() => console.log("Pressed")}
+            />
+            <Text style={styles.buttontext}>Gray Tint</Text>
+          </View>
+        </View>
       </View>
     </PaperProvider>
   );
@@ -34,4 +81,15 @@ const styles = StyleSheet.create({
     height: 300,
     borderRadius: 10,
   },
+  buttontext:{
+    textAlign: "center",
+    fontSize: 8,
+    marginTop: 5,
+    color: "black",
+  },
+  buttoncomponent:{
+    margin: 10,
+    alignItems: "center",
+    borderBlockColor: "black",
+  }
 });
