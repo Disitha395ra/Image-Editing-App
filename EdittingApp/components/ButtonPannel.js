098800ab93b8d2080814d7e5df279f207ebc1984
Disitha <disithar@gmail.com>
2025-01-11 10:58:54 +0530

    Enhance ButtonPannel and Tint components to accept photo as a parameter and update tab bar icons and labels

--- EdittingApp/components/ButtonPannel.js
+++ EdittingApp/components/ButtonPannel.js
@@ -6,37 +6,41 @@ import Download from './Download'
 
 const Tab = createBottomTabNavigator();
 
-export default function ButtonPannel(){
-    return(
-        <Tab.Navigator
-            ScreenOptions={({route})=>({
-                tabBarIcon: ({focused, color, size})=>{
-                    let iconName;
-                    if(route.name === 'Tint'){
-                        iconName = focused ? 'tint' : 'tint-outline'
-                    } else if(route.name === 'Adjust'){
-                        iconName = focused ? 'adjust' : 'adjust-outline'
-                    } else if(route.name === 'Download'){
-                        iconName = focused ? 'download' : 'download-outline'
-                    }
-                    return <Ionicons name={iconName} size={size} color={color}/>
-                },
-                tabBarActiveTintColor: 'tomato',
-                tabBarInactiveTintColor: 'gray',
-            })}
-        >
-            <Tab.Screen
-                name="Tint"
-                component={Tint}
-            />
-            <Tab.Screen
-                name="Adjust"
-                component={Adjust}
-            />
-            <Tab.Screen
-                name="Download"
-                component={Download}
-            />
-        </Tab.Navigator>
+export default function ButtonPannel({route}){
+    const {photo} = route.params;
+    return (
+      <Tab.Navigator
+        ScreenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName;
+            if (route.name === "Tint") {
+              iconName = "brush-variant";
+            } else if (route.name === "Adjust") {
+              iconName = "chart-areasplin";
+            } else if (route.name === "Download") {
+              iconName = "check-underline-circle";
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: "green",
+          tabBarInactiveTintColor: "gray",
+        })}
+      >
+        <Tab.Screen name="Tint" component={Tint} initialParams={{ photo }} options={
+            {tabBarLabel: "Tint"}
+        }/>
+        <Tab.Screen
+          name="Adjust"
+          component={Adjust}
+          initialParams={{ photo }}
+          options={{ tabBarLabel: "Adjust" }}
+        />
+        <Tab.Screen
+          name="Download"
+          component={Download}
+          initialParams={{ photo }}
+          options={{ tabBarLabel: "Download" }}
+        />
+      </Tab.Navigator>
     );
 }
--- EdittingApp/components/Tint.js
+++ EdittingApp/components/Tint.js
@@ -2,12 +2,14 @@ import React from "react";
 import { StyleSheet,View } from "react-native";
 import { PaperProvider, Text } from "react-native-paper";
 
-export default function Tint() {
+export default function Tint({route}) {
+    const {photo} = route.params;
   return (
     <View>
-        <PaperProvider>
-            <Text>This is the Tint screen</Text>
-        </PaperProvider>
+      <PaperProvider>
+        <Text>This is the Tint screen</Text>
+        {photo}
+      </PaperProvider>
     </View>
   );
 }
