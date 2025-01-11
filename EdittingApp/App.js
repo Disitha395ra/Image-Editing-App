c52ff91a40b8d420f945b60da9de6db7075b6538
Disitha <disithar@gmail.com>
2025-01-11 09:11:48 +0530

    Add ButtonPannel component and integrate it into navigation stack; update dependencies

--- EdittingApp/App.js
+++ EdittingApp/App.js
@@ -1,29 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Home from './components/Home'
-import UploadPhoto from './components/UploadPhoto'
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from "@react-navigation/stack";
+import Home from './components/Home'
+import UploadPhoto from './components/UploadPhoto'
+import ButtonPannel from './components/ButtonPannel';
+import { PaperProvider } from 'react-native-paper';
+import { StatusBar } from 'expo-status-bar';
 const Stack = createStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
+    <PaperProvider>
       <NavigationContainer>
         <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen name="Home" component={Home} />
-          <Stack.Screen name="UploadPhoto" component={UploadPhoto} />
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="UploadPhoto"
+            component={UploadPhoto}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="ButtonPannel"
+            component={ButtonPannel}
+            options={{ headerShown: false }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
-    </View>
+      <StatusBar style="auto" />
+    </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
